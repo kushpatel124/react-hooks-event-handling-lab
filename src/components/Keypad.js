d1526e4e5126b14d72154ba30cb63cb2b987eda3
Kush <kushpatel124@gmail.com>
2023-02-11 13:13:07 -0500

    commit tree 3327494d4ecda4c6e42c3cdcf703c9b25a8d7f4b

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleInputChange() {
+        console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleInputChange} />
+        </div>
     )
 }
 
